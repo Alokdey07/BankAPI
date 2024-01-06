c706c13947dcf5e30b8b55540379e527d3705551
Alokdey07 <alokdeykv@gmail.com>
2024-01-07 00:57:43 +0530

    Update code

---
+++ Loans/src/main/java/com/easybytes/loans/constants/LoansConstants.java
@@ -0,0 +1,22 @@
+package com.easybytes.loans.constants;
+
+public final class LoansConstants {
+    private LoansConstants(){
+        // restrict instantiation
+    }
+
+    public static final String HOME_LOAN = "Home Loan";
+    public static final int NEW_LOAN_LIMIT = 1_00_000;
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 ="Loan Created Successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String Status_417= "417";
+
+    public static final String MESSAGE_417_UPDATE = "Update operation failed. please try again or contact Dev team";
+
+    public static final String MESSAGE_417_DELETE = "Delete operation failed. Please Try again or contact Dev team";
+
+
+
+}
