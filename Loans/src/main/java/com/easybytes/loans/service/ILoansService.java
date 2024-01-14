21a051c62ebfb08abf21bb30dd902c4869cd4c8e
Alokdey07 <alokdeykv@gmail.com>
2024-01-14 23:26:58 +0530

    Update code

---
+++ Loans/src/main/java/com/easybytes/loans/service/ILoansService.java
@@ -0,0 +1,10 @@
+package com.easybytes.loans.service;
+
+public interface ILoansService {
+   /* @param mobileNumber - Mobile number of the Customer*/
+    void createLoan(String mobileNumber);
+
+    @param mobileNumber - Input mobile number
+
+
+}
